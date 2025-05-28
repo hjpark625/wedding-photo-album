106f3ce4b3e4060cb2e610ef2121d1660cbbcafb
Heeju Park <zero950@gmail.com>
2025-05-28 19:43:14 +0900

    refactor: 프로젝트 초기 셋팅 완료

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import type { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('@app/home/home.component').then((C) => C.HomeComponent) }
+];
