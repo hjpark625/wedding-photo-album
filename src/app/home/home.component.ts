ba10c4d2c31f490696a10089c42a36cc363dfee7
hjpark625 <zero950@gmail.com>
2025-05-31 11:01:06 +0900

    feat: 이미지 첨부하기 초안 기능 구현 완료

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PhotoRequestService } from '@/apis/photo-request.service';
+
+import type { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  readonly title = signal('Wedding Photo Album');
+  private photoRequestService = inject(PhotoRequestService);
+
+  saveAttachImages(event: Event) {
+    if (event.target instanceof HTMLInputElement) {
+      const target = event.target;
+      const files = Array.from(target.files || []);
+      this.uploadPhotos(files);
+    }
+  }
+
+  uploadPhotos(images: File[]) {
+    if (images.length === 0) return;
+
+    this.photoRequestService.uploadPhotos(images).subscribe({
+      next: (response) => {
+        console.log('Upload successful:', response.imageUrls);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+      }
+    });
+  }
 }
