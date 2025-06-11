e4173b2bed6e8f074cb8d52cdc70c1d532ccd30a
hjpark625 <zero950@gmail.com>
2025-06-11 23:22:19 +0900

    refactor: 컴포넌트 마운트시 토큰 발급 라이프사이클 추가

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, viewChild } from '@angular/core';
+
 import { PhotoRequestService } from '@/apis/photo-request.service';
+import { AuthRequestService } from '@/apis/auth-request.service';
 
 import type { HttpErrorResponse } from '@angular/common/http';
+import type { ElementRef, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   private photoRequestService = inject(PhotoRequestService);
+  private authRequestService = inject(AuthRequestService);
+
+  // TODO: CSRF 토큰 저장 및 관리 방식 고민 필요
+  private csrfToken = '';
+
+  readonly imageUploadInput = viewChild<ElementRef<HTMLInputElement>>('imageUploadInput');
 
   saveAttachImages(event: Event) {
     const formData = new FormData();
@@ -24,14 +33,34 @@ export class HomeComponent {
     }
   }
 
+  onInputTrigger(e: Event) {
+    e.preventDefault();
+
+    const inputElement = this.imageUploadInput();
+
+    if (!inputElement) return;
+    inputElement.nativeElement.click();
+  }
+
   uploadPhotos(formData: FormData) {
-    this.photoRequestService.uploadPhotos(formData).subscribe({
+    this.photoRequestService.uploadPhotos(formData, this.csrfToken).subscribe({
       // TODO: 응답 및 에러에 관련된 핸들링 추가 필요
       next: (response) => {
         console.log('Upload successful:', response.imageUrls);
       },
       error: (error: HttpErrorResponse) => {
         console.error(error);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.authRequestService.getCsrfToken().subscribe({
+      next: (response) => {
+        this.csrfToken = response.csrfToken;
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error('Error fetching CSRF token:', error);
       }
     });
   }
