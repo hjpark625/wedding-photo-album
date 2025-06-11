ed46376d3e1b57955f6e7cbd8ac1ed4fe4ed3d4b
hjpark625 <zero950@gmail.com>
2025-06-11 23:21:33 +0900

    refactor: 기존 사진 업로드 api 내 헤더, 쿠키 csrf 토큰 추가

--- src/apis/photo-request.service.ts
+++ src/apis/photo-request.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { environment } from '@/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import type { UploadImagesResponse } from '@/apis/types/photo-request.type';
 
@@ -11,7 +11,10 @@ export class PhotoRequestService {
   private http = inject(HttpClient);
   private readonly apiUrl = environment.API_SERVER;
 
-  uploadPhotos(formData: FormData) {
-    return this.http.post<UploadImagesResponse>(`${this.apiUrl}image/upload`, formData);
+  uploadPhotos(formData: FormData, csrfToken: string) {
+    return this.http.post<UploadImagesResponse>(`${this.apiUrl}image/upload`, formData, {
+      headers: new HttpHeaders({ 'x-csrf-token': csrfToken }),
+      withCredentials: true
+    });
   }
 }
