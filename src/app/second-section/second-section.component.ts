1c1479514a34de69076e31a070f8453bfc4fa66a
hjpark625 <zero950@gmail.com>
2025-06-20 00:25:08 +0900

    feat: 2번 섹션 컴포넌트 분리

---
+++ src/app/second-section/second-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'second-section',
+  templateUrl: './second-section.component.html',
+  styleUrl: './second-section.component.scss'
+})
+export class SecondSectionComponent {}
